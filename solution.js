80c2e961c837c281b1882463e4c5beeaf25c6968
Tasman Terzakes Drake <tasman.drake@colorado.edu>
2017-05-24 14:02:12 -0600

    Completed build and burn

--- solution.js
+++ solution.js
@@ -1,9 +1,11 @@
-function hash(plaintext) {
+const bcrypt = require('bcrypt');
 
+function hash(plaintext) {
+  return bcrypt.hashSync(plaintext, 8);
 }
 
 function compare(plaintext, hash) {
-
+  return bcrypt.compareSync(plaintext, hash);
 }
 
 module.exports = {
